2ec63a903b61743803ba919bdbbeee08a3d2271f
Abel Seifu <abel.workmail@gmail.com>
2022-09-29 16:49:29 +0300

    Add configuration for lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ci: {
+      collect: {
+        /* Add configuration here */
+        startServerCommand: 'npm start',
+        url: ['http://localhost:8080'],
+        numberOfRuns: 1,
+        /* Add configuration here */
+      },
+    },
+  };
